26f088462a641afa8e8e887b30cb1f578b82248a
Vanshika Goel <65799147+vanshika-goel0109@users.noreply.github.com>
2020-09-26 11:16:47 +0530

    Update assis.py

--- assis.py
+++ assis.py
@@ -1,1 +1,19 @@
+import speech_recognition as sr
+import webbrowser as wb
 
+r2 = sr.Recognizer()
+
+with sr.Microphone() as source:
+    r2.adjust_for_ambient_noise(source, duration=5)
+    print('speak now...')
+    audio = r2.listen(source)
+    print(r2.recognize_google(audio))
+
+def func():
+    with sr.Microphone() as source:
+        r2.adjust_for_ambient_noise(source, duration=5)
+        print('search your query')
+        audio = r2.listen(source)
+
+    get = r2.recognize_google(audio)
+    print(get)
