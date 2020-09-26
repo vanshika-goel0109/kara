e969577b9c11b5f1313960100dc9901a9b871027
Vanshika Goel <65799147+vanshika-goel0109@users.noreply.github.com>
2020-09-26 12:49:52 +0530

    Update remindShedule.py

--- remindShedule.py
+++ remindShedule.py
@@ -1,13 +1,29 @@
 from datetime import datetime
-import os
 from gtts import gTTS
+import speech_recognition as sr
+import webbrowser as wb
+import os
 
 language='en'
+r2 = sr.Recognizer()
 
 def getData():
     # Write the code to get the date and purpose here
     # Store them in date and pupose respectively
     # both must be of str type when they get stored
+    
+   with sr.Microphone() as source:#stores the purpose 
+     r2.adjust_for_ambient_noise(source, duration=5)
+     print('tell me the event please')
+     purpose = r2.listen(source)
+     print(r2.recognize_google(purpose))
+    
+   with sr.Microphone() as source:#stores the date
+     r2.adjust_for_ambient_noise(source, duration=5)
+     print('tell me date')
+     date = r2.listen(source)
+     print(r2.recognize_google(date))
+ 
 
 def store(date, purpose):
     f = open("sched.txt","a")
@@ -25,4 +41,3 @@ def read(schedList):
             output=gTTS(text=purpose,lang=language,slow=false)
             output.save("output.mp3")
             os.system("start output.mp3")
-            
