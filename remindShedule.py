aab03e5d5bb3f1e8e2d35494d97df4c34012c496
Vanshika Goel <65799147+vanshika-goel0109@users.noreply.github.com>
2020-09-26 12:11:31 +0530

    Update remindShedule.py

--- remindShedule.py
+++ remindShedule.py
@@ -3,17 +3,36 @@ import os
 import speech_recognition as sr
 import webbrowser as wb
 
+r2 = sr.Recognizer()
+
+func()
 
 # Function to Store Appointments
+
 def store(date, purpose):
     f = open("sched.txt","a")
     f.append(date+"#"+purpose)
 
 def read(schedList):
+    
     now=datetime.now()
     now=now.strftime("%d%m%Y %H:%M")
+    
     for sched in schedList:
         if now in sched:
+            #inputs for meeting reminder
             # The assistant telling the purpose part comes here
-
-
+            
+            with sr.Microphone() as source:
+            r2.adjust_for_ambient_noise(source, duration=5)
+            print('tell me the event please')
+            purpose = r2.listen(source)
+            print(r2.recognize_google(purpose))
+            
+             # The assistant telling the date part comes here
+            with sr.Microphone() as source:
+            r2.adjust_for_ambient_noise(source, duration=5)
+            print('tell me date')
+            date = r2.listen(source)
+            print(r2.recognize_google(date))
+ 
