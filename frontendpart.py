9d02777ea55ec4943a1c30e01317e8049ef3e3aa
Vanshika Goel <65799147+vanshika-goel0109@users.noreply.github.com>
2020-09-27 08:32:15 +0530

    Update frontendpart.py

--- frontendpart.py
+++ frontendpart.py
@@ -13,37 +13,39 @@ class myLayout(GridLayout):
         super().__init__(**kwargs)
         self.cols=1
 
-        self.add_widget(AsyncImage(source="https://img.pngio.com/assistance-png-5-png-image-assistance-png-800_600.png"))
+        self.add_widget(AsyncImage(source="https://img.pngio.com/assistance-png-5-png-image-assistance-png-800_600.png"))#image we need to present
         self.bottom_layout=FloatLayout(size=(600,600))
-
-        self.lets_start=Button(
-            text="Lets Start",
+        
+        #button to start meeting reminder.
+        self.meeting=Button(
+            text="Meeting Reminder",
             background_color=(1,0.5,0.5,2),
             size_hint=(0.2,0.2),
             pos_hint={'x':0.3,'y':0.5}
              )
 
-        self.lets_start.bind(on_press=self.lets_start_callback)
-        self.bottom_layout.add_widget(self.lets_start)
-
-        self.close=Button(
-            text="Close",
+        self.meeting.bind(on_press=self.meeting_callback)
+        self.bottom_layout.add_widget(self.meeting)
+        
+        #button to start halthcare(if we add that or any other thing we can ad here)
+        self.medicine=Button(
+            text="Medicine Reminder",
             background_color=(1,0.5,0.5,2),
             size_hint=(0.2,0.2),
             pos_hint={'x':0.5,'y':0.5}
              )
 
-        self.close.bind(on_press=self.close_callback)
-        self.bottom_layout.add_widget(self.close)
+        self.medicine.bind(on_press=self.medicine_callback)
+        self.bottom_layout.add_widget(self.medicine)
 
         self.add_widget(self.bottom_layout)
-
-    def lets_start_callback(self,instance):
-        x=input("give")
-        print(x)
-
-    def close_callback(self,instance):
-        print("close")
+    
+    #this is function for meeting reminder one,add here you backend code that need to be perform
+    def meeting_callback(self,instance):
+       
+    #this is for medicine one
+    def medicine_callback(self,instance):
+        
 
 class MykivyApp(App):
 
